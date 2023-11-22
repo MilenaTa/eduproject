32947fa5abb42d3f7959f3cbd2ac5b9a941c868d
MilenaT <[200~milena.tanasijevic@htecgroup.com>
2023-11-22 13:16:14 +0100

    UserController master

--- src/main/java/rs/edu/shipping/user/controller/UserController.java
+++ src/main/java/rs/edu/shipping/user/controller/UserController.java
@@ -2,14 +2,10 @@ package rs.edu.shipping.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import rs.edu.shipping.user.User;
-import rs.edu.shipping.user.constants.UserRole;
 import rs.edu.shipping.user.service.UserService;
 
 @RestController
