9701ea2cc8bfbe98512fee55fdc86edff446bc92
MilenaT <[200~milena.tanasijevic@htecgroup.com>
2023-11-22 13:17:10 +0100

    AuthoritiesId test-branch

--- src/main/java/rs/edu/shipping/user/entity/AuthoritiesId.java
+++ src/main/java/rs/edu/shipping/user/entity/AuthoritiesId.java
@@ -7,7 +7,6 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import rs.edu.shipping.user.constants.UserRole;
 
 @Embeddable
 @Getter
